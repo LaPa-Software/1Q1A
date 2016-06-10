47c75aa425fffda868c6aa698f042df805b71ec7
Владислав Пономарев <sys.system@mail.ru>
2016-06-10 13:54:19 +0500

    upd 10

--- platforms/browser/js/io.js
+++ platforms/browser/js/io.js
@@ -3,25 +3,44 @@
         target=target||APP.CONF.server.apiUrl;
         data=data||{};
         data='?query='+JSON.stringify(data);
-        /*var xhr = new XMLHttpRequest();
+        var xhr = new XMLHttpRequest();
         xhr.open('GET', target+data, true);
-        xhr.responseType = 'text';
-        xhr.onload = function(e) {
-            var response=this.response;
-            if (this.status == 200) {
-                if(!plainData){
-                    try{
-                        response=JSON.parse(this.response);
-                    }catch(e){
+        if(xhr.onload) {
+            xhr.onload = function (e) {
+                var response = this.response;
+                if (this.status == 200) {
+                    if (!plainData) {
+                        try {
+                            response = JSON.parse(this.response);
+                        } catch (e) {
 
+                        }
                     }
+                } else {
+                    response = false;
                 }
-            }else{
-                response=false;
-            }
-            if(onFinish)onFinish(response);
-        };
-        xhr.send();*/
+                if (onFinish)onFinish(response);
+            };
+        }else {
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    var response = xhr.responseText;
+                    if (xhr.status == 200) {
+                        if (!plainData) {
+                            try {
+                                response = JSON.parse(xhr.responseText);
+                            } catch (e) {
+
+                            }
+                        }
+                    } else {
+                        response = false;
+                    }
+                    if (onFinish)onFinish(response);
+                }
+            };
+        }
+        xhr.send();
     };
     function initIoLib() {
         window.APP.io=io;
