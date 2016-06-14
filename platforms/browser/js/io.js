a5089c2af0725b163e4a3969cd9c1aadb9a525e2
Владислав Пономарев <sys.system@mail.ru>
2016-06-14 07:05:51 +0500

    upd 19 dbg 2

--- platforms/browser/js/io.js
+++ platforms/browser/js/io.js
@@ -5,8 +5,10 @@
         data = '?query=' + JSON.stringify(data);
         var xhr = new XMLHttpRequest();
         xhr.open('GET', target + data, true);
+        alert('request to '+target + data);
         xhr.responseType = 'text';
         xhr.onload = function (e) {
+            alert('request finished with status '+this.status);
             var response = this.response;
             if (this.status == 200) {
                 if (!plainData) {
@@ -22,6 +24,7 @@
             if (onFinish)onFinish(response);
         };
         xhr.send();
+        alert('request send');
     };
     function initIoLib() {
         window.APP.io=io;
