ceaf9eed6fc8699928834b9411bdd8d7ab044ef8
Владислав Пономарев <sys.system@mail.ru>
2016-07-12 06:11:50 +0500

    Clean

--- platforms/browser/js_old/user.js
+++
@@ -1,9 +0,0 @@
-;(function() {
-    var fetchAccount=function (data) {
-
-    };
-    function initUserLib() {
-        window.APP.fetchAccount=fetchAccount;
-    }
-    APP.HOOK.reg('PreInit',false,initUserLib,'user.js');
-})();
